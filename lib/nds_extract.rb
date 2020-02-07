7d20d57998983e1c545134477aeafe67d50a90dc
Diana Ponce <dianap2341@gmail.com>
2020-02-07 23:58:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,11 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-require 'pp'
+require 'pp'# why can i not put total in beging why inb this section(when put in the beginning i got different numbers ?? 
 def directors_totals(nds)
 result = []
     r_index=0 
       while r_index < nds.length 
-     i=0 # why can i not put total in beging why inb this section(when put in the beginning i got different numbers ?? 
+     i=0 
       total=0 
 
        n= nds[r_index][:name]
