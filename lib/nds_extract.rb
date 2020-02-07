4460f3a206c96f860c66b82b3a747e3ddc2b29b3
Diana Ponce <dianap2341@gmail.com>
2020-02-07 23:57:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,25 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
 def directors_totals(nds)
-def directors_totals(nds)
-result = {}
-   total=0 
+result = []
     r_index=0 
       while r_index < nds.length 
        i=0
-       puts nds[r_index][:name]
+      total=0 
+
+       n= nds[r_index][:name]
        t= nds[r_index][:movies]
          while i < t.length
           
-          puts t[i][:worldwide_gross]
+          k= t[i][:worldwide_gross]
+          total += k
+          
                 i += 1
               end
               r_index += 1
-              nil
+              result << [n , total]
             end
+            result.to_h
           end
-        end
+
+ 
